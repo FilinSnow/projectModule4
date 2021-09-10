6e98758c32e235a005e1ad92516fa9bdf2592853
Vlad <vproskurin.exceedteam@gmail.com>
2021-09-10 12:14:36 +0300

    Add 1 component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import Header from "./Components/Header/Header";
+
 import './App.scss';
 
 const App = () => {
   return (
       <div className="App">
+        <Header/>
       </div>
   );
 }
